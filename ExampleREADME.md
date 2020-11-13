aa65b1170a13ef6ffa61c737223a7e8b48ccf2a0
Shane Kraycs <smkraycs13@yahoo.com>
2020-11-12 21:36:29 -0500

    final commit

---
+++ ExampleREADME.md
@@ -0,0 +1,50 @@
+# readMe-Generator
+
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/smkraycs12/undefined?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/smkraycs12/undefined?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  *The what, why, and how:* 
+  
+  Project Description
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  test
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  test
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  test
+  
+  ## License
+  
+  Unlicensed
+  
+  ---
+  
+  ## Questions?
+    
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@smkraycs12](https://api.github.com/users/smkraycs12)
+  
--- index.js
+++ index.js
@@ -1,16 +1,128 @@
+// required external packages 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// required internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'username',
+        default: 'smkraycs12',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of the GitHub Repository?",
+        name: 'repository',
+        default: '09_ReadMe_Generator',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid name for the Repository is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        mesasge: "What is the title of the Project?",
+        name: 'title',
+        default: 'readMe-Generator',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'Unlicensed'],
+        name: 'license'
+    }
 
 ];
 
-// function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
 }
 
-// function to initialize program
-function init() {
+const writeFileAsync = util.promisify(writeToFile);
 
-}
 
-// function call to initialize program
+// Main function
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 init();
